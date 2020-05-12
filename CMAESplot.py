a6880c4d6b52dfd19efd094e6d3c16693777428d
julesbertrand <julesbertrand13@gmail.com>
2020-05-11 23:35:55 -0700

    CMAES plot with covariance ellipse

--- CMAESplot.py
+++ CMAESplot.py
@@ -1,7 +1,29 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 import numpy as np
+from matplotlib.patches import Ellipse
+import matplotlib.transforms as transforms
 
+def confidence_ellipse(cov, xmean, ax, facecolor='none', **kwargs):
+    """
+    from https://matplotlib.org/devdocs/gallery/statistics/confidence_ellipse.html  
+    Create a plot of the covariance confidence ellipse of *x* and *y*.
+    """
+    pearson = cov[0, 1]/np.sqrt(cov[0, 0] * cov[1, 1])
+    ell_radius_x = np.sqrt(1 + pearson)
+    ell_radius_y = np.sqrt(1 - pearson)
+    ellipse = Ellipse((0, 0), width=ell_radius_x * 2, height=ell_radius_y * 2,
+                      facecolor=facecolor, **kwargs)
+
+    scale_x = np.sqrt(cov[0, 0]) * 3
+    scale_y = np.sqrt(cov[1, 1]) * 3
+    transf = transforms.Affine2D() \
+        .rotate_deg(45) \
+        .scale(scale_x, scale_y) \
+        .translate(xmean[0], xmean[1])
+
+    ellipse.set_transform(transf + ax.transData)
+    return ax.add_patch(ellipse)
 
 class GeneticDFO():
 
@@ -98,6 +120,7 @@ class GeneticDFO():
 
                 """ Adapt covariance matrix """
                 artmp = (1 / self.sigma) * arx - np.tile(xold, (1, self.mu))
+                self.C_prev = self.C
                 self.C = (1 - self.c1 - self.cmu) * self.C + self.c1 * (np.dot(self.pc, self.pc.T) +
                                                                         (1 - hsig) * self.cc * (2 - self.cc) * self.C) + self.cmu * np.dot(np.dot(artmp, np.diag((self.weights).T[0])), artmp.T)
                 """ Adapt sigma """
@@ -113,13 +136,15 @@ class GeneticDFO():
                     self.invsqrtC = np.dot(np.dot(self.B, np.diag((1 / self.D).T[0])), self.B.T)
                 if not i + l:
                     self.ax[i][l].plot(self.xmean[0], self.xmean[1], 'ro', label="New point", ms=self.ms)
+                    confidence_ellipse(self.C_prev, xold, self.ax[i][l], edgecolor='green', label = 'Cov matrix')
                 else:
                     self.ax[i][l].plot(self.xmean[0], self.xmean[1], 'ro', ms=self.ms)
+                    confidence_ellipse(self.C_prev, xold, self.ax[i][l], edgecolor='green')
                 self.ax[i][l].set_title("Itération n°" + str(count))
                 # self.ax[i][l].legend(loc=2, fontsize=8)
                 self.ax[i][l].set(adjustable='box', aspect='equal')
                 self.sigmas += [self.sigma]
-            self.fig.legend(ncol=3, loc='upper center', bbox_to_anchor=(0.5, 0.95), prop={'size': 9})
+            self.fig.legend(ncol=4, loc='upper center', bbox_to_anchor=(0.5, 0.95), prop={'size': 9})
 
 
 if __name__ == '__main__':
